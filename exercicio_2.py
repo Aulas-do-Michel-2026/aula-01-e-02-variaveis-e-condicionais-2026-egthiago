78a130c563788c741349443b50b8ea77a9c6bac6
egthiago <thiago.eg61@gmail.com>
2026-03-28 19:30:53 -0300

    Add dose calculation based on patient weight

--- exercicio_2.py
+++ exercicio_2.py
@@ -13,3 +13,8 @@ Digite o peso do paciente (em kg):
 Resposta:
 Média: 140 mg
 """
+
+peso = float(input('Digite o peso do paciente (kg): '))
+dose = 2 * peso
+
+print(f'A dose do paciente é de {dose} mg.')
