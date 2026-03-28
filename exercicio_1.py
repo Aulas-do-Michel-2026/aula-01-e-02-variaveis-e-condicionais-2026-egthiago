b596d38d7b4788de4cb98c1544aa086830cd8630
egthiago <thiago.eg61@gmail.com>
2026-03-28 19:22:53 -0300

    Implement grade input and average calculation
    Added functionality to receive three grades and calculate the average.

--- exercicio_1.py
+++ exercicio_1.py
@@ -1,4 +1,4 @@
-"""
+'''
 #### Exercício 1
 
 Receba três notas (números decimais) de um aluno e imprima a média.
@@ -18,4 +18,13 @@ Média: 8.17
 Dica: Use inputs para receber os dados! 
 Lembre de converter ele para o tipo necessário!
 Print na tela com "print"
-"""
+'''
+
+
+nota_1 = float(input('Insira a primeira nota: '))
+nota_2 = float(input('Insira a segunda nota: '))
+nota_3 = float(input('Insira a terceira nota: '))
+
+media = (nota_1 + nota_2 + nota_3)/3
+
+print(f'A média do aluno foi {media}')
